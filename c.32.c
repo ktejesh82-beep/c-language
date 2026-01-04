3ab137ac7871853c64a59ae0570085da1091cffb
ktejesh82-beep <ktejesh82@gmail.com>
2026-01-04 22:03:57 +0530

    Add files via upload

---
+++ c.32.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int n,i,arr[100],sum=0;
+    printf("enter the value:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+        sum+=arr[i];
+    }
+    printf("sum of Array elements: ");
+    printf("%d",sum);
+    
+    return 0;
+}
